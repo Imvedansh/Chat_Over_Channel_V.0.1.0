dbb96cdc42f286fa8ac3d7a192d226f1a53ef9e6
Imvedansh <ved17007@gmail.com>
2025-02-21 20:37:31 +0530

    Deploy to Heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import io from "socket.io-client";
 import { useEffect, useState } from "react";
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://server-build.herokuapp.com");
 function App() {
   const [room, setRoom] = useState("");
   const [message, setMessage] = useState("");
